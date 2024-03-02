07888641a2f699cebf5b14b0840e48360defb32b
jarahtech@gmail.com <jarahtech@gmail.com>
2024-03-02 15:42:08 +0600

    first commit

---
+++ include/config.php
@@ -0,0 +1,22 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mahabub'] = array ('1'=>'mahabub<|<admin','mahabub>|>eHFyqpKlnZ1yeHE=');
+
+
+
+
+
+
+$data['CAMP-14-R'] = array ('1'=>'CAMP-14-R!he608mxz84b.sn.mynetname.net','CAMP-14-R@|@shafi','CAMP-14-R#|#eHE=','CAMP-14-R%CAMP-14-R','CAMP-14-R^login.net','CAMP-14-R&QR','CAMP-14-R*10','CAMP-14-R(1','CAMP-14-R)','CAMP-14-R=10','CAMP-14-R@!@enable');
+
+
+
+
+$data['STREET-3-LEFT'] = array ('1'=>'STREET-3-LEFT!hca08ag5kyd.sn.mynetname.net','STREET-3-LEFT@|@shafi','STREET-3-LEFT#|#eHE=','STREET-3-LEFT%STREET 3 LEFT','STREET-3-LEFT^login.net','STREET-3-LEFT&QR','STREET-3-LEFT*10','STREET-3-LEFT(1','STREET-3-LEFT)','STREET-3-LEFT=10','STREET-3-LEFT@!@enable');
+$data['CAMP-26W-L'] = array ('1'=>'CAMP-26W-L!hd908fjd827.sn.mynetname.net','CAMP-26W-L@|@mahmud','CAMP-26W-L#|#eZOVnHFma2Nj','CAMP-26W-L%CAMP-26W_L','CAMP-26W-L^login.net','CAMP-26W-L&AED','CAMP-26W-L*10','CAMP-26W-L(1','CAMP-26W-L)','CAMP-26W-L=10','CAMP-26W-L@!@enable');
+$data['CAMP-30'] = array ('1'=>'CAMP-30!hc807sa2fhx.sn.mynetname.net','CAMP-30@|@shafi','CAMP-30#|#eHE=','CAMP-30%CAMP-30','CAMP-30^login.net','CAMP-30&QR','CAMP-30*10','CAMP-30(1','CAMP-30)','CAMP-30=10','CAMP-30@!@enable');
+$data['CAMP-16'] = array ('1'=>'CAMP-16!heb08tcbg4n.sn.mynetname.net','CAMP-16@|@shafi','CAMP-16#|#eHE=','CAMP-16%CAMP-16','CAMP-16^login.net','CAMP-16&QR','CAMP-16*10','CAMP-16(1','CAMP-16)','CAMP-16=10','CAMP-16@!@enable');
+$data['CAMP-47-L'] = array ('1'=>'CAMP-47-L!he508hw0wfc.sn.mynetname.net','CAMP-47-L@|@shafi','CAMP-47-L#|#eHE=','CAMP-47-L%CAMP-47-L','CAMP-47-L^login.net','CAMP-47-L&QR','CAMP-47-L*10','CAMP-47-L(1','CAMP-47-L)','CAMP-47-L=10','CAMP-47-L@!@enable');
+$data['CAMP-26-R'] = array ('1'=>'CAMP-26-R!hen08gm9hbn.sn.mynetname.net','CAMP-26-R@|@shafi','CAMP-26-R#|#eHE=','CAMP-26-R%CAMP-26-R','CAMP-26-R^login.net','CAMP-26-R&QR','CAMP-26-R*10','CAMP-26-R(1','CAMP-26-R)','CAMP-26-R=10','CAMP-26-R@!@enable');
+$data['CAMP-10-A'] = array ('1'=>'CAMP-10-A!hee08ydy7rb.sn.mynetname.net','CAMP-10-A@|@shafi','CAMP-10-A#|#eHE=','CAMP-10-A%CAMP-10-A','CAMP-10-A^login.net','CAMP-10-A&QR','CAMP-10-A*10','CAMP-10-A(1','CAMP-10-A)','CAMP-10-A=10','CAMP-10-A@!@enable');
+$data['CAMP-14-E'] = array ('1'=>'CAMP-14-E!hck084f341b.sn.mynetname.net','CAMP-14-E@|@shafi','CAMP-14-E#|#eHE=','CAMP-14-E%CAMP-14-E','CAMP-14-E^login.net','CAMP-14-E&QR','CAMP-14-E*10','CAMP-14-E(1','CAMP-14-E)','CAMP-14-E=10','CAMP-14-E@!@enable');
